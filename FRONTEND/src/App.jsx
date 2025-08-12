ab5c9522b2f62ec3a5b2d26a5cceeca97510f101
hieunn-dev <nguyenngochieu0323@gmail.com>
2025-08-12 15:31:27 +0700

    hieunn

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './components/HomePage/HomePage';
+function App() {
+  return (
+    <div >
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
