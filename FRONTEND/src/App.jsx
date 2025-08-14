e3ca32e8a87a5f752d04dc354569a8886d9a0b6e
hieunn-dev <nguyenngochieu0323@gmail.com>
2025-08-14 16:15:38 +0700

    hieunn

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './components/HomePage/HomePage';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
-    <div >
+    <div className="app-wrapper">
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
+          <Route path='/' element={<HomePage />} />
         </Routes>
+        {/* <Footer /> */}
       </BrowserRouter>
     </div>
   );
